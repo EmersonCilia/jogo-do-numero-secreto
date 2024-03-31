fbb323a99f709b971ef756cf051ad1f18a61b60f
emersoncilia <emersonwpprado@gmail.com>
2024-03-31 12:17:37 -0300

    voltando para o original de 10 numeros

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroMaximo = parseInt(100);
+let numeroMaximo = parseInt(10);
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 MensagemInicial();
